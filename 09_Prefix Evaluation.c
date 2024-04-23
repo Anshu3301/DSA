514d4c736054331515be5cbd0d126fef9d48586f
Anshu Sain <134282893+Anshu3301@users.noreply.github.com>
2024-04-24 00:34:20 +0530

    Update 09_Prefix Evaluation.c

--- 09_Prefix Evaluation.c
+++ 09_Prefix Evaluation.c
@@ -1,1 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+int stack[100];    // Globally declared
+int top = -1;
+
+void push(int x){
+  stack[++top] = x;
+}
+
+int pop(){
+  return stack[top--];
+}
+
+int evaluate(char *prefix){
+  int i = strlen(prefix) - 1;
+  while(i >= 0){
+    if(isdigit(prefix[i])){
+      push(prefix[i] - '0');   // converting to int & push
+    }
+    else{                      // For operator
+      int x = pop();
+      int y = pop();
+
+      switch(prefix[i]){
+        case '+': {
+          push(x+y); 
+          break;
+        }
+        case '-': {
+          //printf("x=%d y=%d\n",x,y);
+          push(x-y); 
+          break;
+        }
+        case '*': {
+          //printf("x=%d y=%d\n",x,y);
+          push(x*y); 
+          break;
+        }
+        case '/': {
+          //printf("x=%d y=%d\n",x,y);
+          push(x/y); 
+          break;
+        }
+        case '^': {
+          //printf("x=%d y=%d\n",x,y);
+          push(pow(x,y)); 
+          break;
+        }
+      }
+    }
+    i--;
+  }
+  return pop();          // Popping final Answer i.e. Stack[top]
+}
+
+
+int main() {
+
+  char prefix[100];  
+  printf("Enter Prefix Expression: ");
+  scanf("%s",prefix);
+
+  printf("Answer: %d",evaluate(prefix));
+  
+  return 0;
+}
